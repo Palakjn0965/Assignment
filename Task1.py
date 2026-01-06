4edff4f15da44c1e697a67c2393443b33e00ac82
codeversify-cell <palakjn9879@gmail.com>
2026-01-06 11:27:42 +0530

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+Add = a + b
+Sub = a - b
+Mul = a * b
+Div = a / b
+print("Addition: ", Add)
+print("Subtraction: ", Sub)
+print("Multiplication: ", Mul)
+print("Division: ", Div)
